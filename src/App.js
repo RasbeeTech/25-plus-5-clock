60545e796cf58d720b0baf237ece4671fe3c8560
MykalB <mykalb081@gmail.com>
2021-06-06 21:47:39 -0400

    Bug Fixes
    - Fixed: Audio playing a second after time reach 00:00
    - Fixed: Timer label format when incrementing, decrementing, or changing sessions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
+import beepAudio from './beep.mp3'
 
-const AUDIO = 'https://sampleswap.org/samples-ghost/SOUND%20EFFECTS%20and%20NOISES/Bleeps%20Blips%20Blonks%20Blarts%20and%20Zaps/463[kb]beep-beep-bopbop-bop-bop.wav.mp3'
 
 class App extends React.Component {
   constructor(props){
@@ -26,7 +26,9 @@ class App extends React.Component {
     this.timer = 0;
   }
   reset(){
-    document.getElementById('beep').pause();
+    let beep = document.getElementById('beep');
+    beep.pause();
+    beep.currentTime = 0;
     clearInterval(this.timer);
     this.setState({
       breakLength: 5,
@@ -41,25 +43,41 @@ class App extends React.Component {
     this.setState(() => {
       let currentValue = this.state.breakLength;
       let newValue = currentValue <= 1 ? 1 : currentValue - 1;
-      return {breakLength: newValue};
+      if(!this.state.startStop && this.state.timerLabel == 'Break'){
+        return {
+          breakLength: newValue,
+          minLeft: ('0' + newValue).slice(-2),
+          secLeft: '00'
+        };
+      } else {
+        return {breakLength: newValue};
+      }
     });
   }
   breakIncrement(){
     this.setState(() => {
       let currentValue = this.state.breakLength;
       let newValue = currentValue >= 60 ? 60 : currentValue + 1;
-      return {breakLength: newValue};
+      if(!this.state.startStop && this.state.timerLabel == 'Break'){
+        return {
+          breakLength: newValue,
+          minLeft: ('0' + newValue).slice(-2),
+          secLeft: '00'
+        };
+      } else {
+        return {breakLength: newValue};
+      }
     });
   }
   sessionDecrement(){
     this.setState(() => {
       let currentValue = this.state.sessionLength;
       let newValue = currentValue <= 1 ? 1 : currentValue - 1;
-      if(!this.state.startStop){
+      if(!this.state.startStop && this.state.timerLabel == 'Session'){
         return {
           sessionLength: newValue,
-          minLeft: newValue,
-          secLeft: 0
+          minLeft: ('0' + newValue).slice(-2),
+          secLeft: '00'
         };
       } else {
         return {sessionLength: newValue};
@@ -70,11 +88,11 @@ class App extends React.Component {
     this.setState(() => {
       let currentValue = this.state.sessionLength;
       let newValue = currentValue >= 60 ? 60 : currentValue + 1;
-      if(!this.state.startStop){
+      if(!this.state.startStop && this.state.timerLabel == 'Session'){
         return {
           sessionLength: newValue,
-          minLeft: newValue,
-          secLeft: 0
+          minLeft: ('0' + newValue).slice(-2),
+          secLeft: '00'
         };
       } else {
         return {sessionLength: newValue};
@@ -95,16 +113,16 @@ class App extends React.Component {
   updateTimer(){
     if(this.state.startStop){
       this.setState(prevState => {
-        if(prevState.secLeft === '00'){
-          if(prevState.minLeft ==='00'){
-            this.beep();
-            this.changeTimer();
-          } else {
+        if(prevState.minLeft === '00' && prevState.secLeft == '01'){
+          this.beep();
+        }
+        if(prevState.minLeft === '00' && prevState.secLeft == '00'){
+          this.changeTimer();
+        } else if(prevState.secLeft === '00'){
             return{
               minLeft: ('0' + (Number(prevState.minLeft) - 1)).slice(-2),
               secLeft: '59'
             };
-          }
         } else {
           return {secLeft: ('0' + (Number(prevState.secLeft) - 1)).slice(-2)};
         }
@@ -115,13 +133,13 @@ class App extends React.Component {
     if(this.state.timerLabel == 'Session'){
       this.setState({
         timerLabel: 'Break',
-        minLeft: this.state.breakLength.toString(),
+        minLeft: ('0' + this.state.breakLength).slice(-2),
         secLeft: '00'
       });
     } else{
       this.setState({
         timerLabel: 'Session',
-        minLeft: this.state.sessionLength.toString(),
+        minLeft: ('0' + this.state.sessionLength).slice(-2),
         secLeft: '00'
       });
     }
@@ -142,10 +160,10 @@ class App extends React.Component {
         <div id='break-length'>{this.state.breakLength}</div>
         <div id='session-length'>{this.state.sessionLength}</div>
         <div id='timer-label'>{this.state.timerLabel}</div>
-        <div id='time-left'>{this.state.minLeft}:{this.state.secLeft}</div>
+        <div id='time-left'>{this.state.minLeft + ':' + this.state.secLeft}</div>
         <button id='start_stop' onClick={this.startStop}/>
         <button id='reset' onClick={this.reset}/>
-        <audio id='beep' src={AUDIO}/>
+        <audio id='beep' src={beepAudio}/>
       </div>
     );
   }
